bc9900c3682f52e234a2f8bb5420233c85a32f31
Cyri0 <nemestamas94@gmail.com>
2020-10-06 18:19:25 +0200

    vegso simitasok, kesz a program

--- script.js
+++ script.js
@@ -12,9 +12,12 @@ function getData(){
 
             image_holder.innerHTML +=
             '<div class="col-sm-6 col-md-4">'+
-            '<div class="picture" id="'+id+'"></div>'+
+            '<div class="picture" id="'+id+'">'+
+            data[i].hashtags+
+            '<br><b>'+data[i].likes +' &#10084;</b>'+
+            '</div>'+
             '</div>';
-            
+
             document.getElementById(id).style.backgroundImage = "url('pics/"+data[i].image+"')";
         }
     })
