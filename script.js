fe22532a244eb66efbb2957ac4c7664e7d046f29
Cyri0 <nemestamas94@gmail.com>
2020-10-06 17:40:01 +0200

    images,json, ROCK

---
+++ script.js
@@ -0,0 +1,23 @@
+let post_number = document.getElementById("post_num");
+let image_holder = document.getElementById("image-holder");
+
+function getData(){
+    $.getJSON("data.json",function(json){
+        let data = json.data;
+        post_number.innerHTML = data.length;
+
+        for (let i = 0; i < data.length; i++) {
+        
+            let id = "pics" + i;
+
+            image_holder.innerHTML +=
+            '<div class="col-sm-6 col-md-4">'+
+            '<div class="picture" id="'+id+'"></div>'+
+            '</div>';
+            
+            document.getElementById(id).style.backgroundImage = "url('pics/"+data[i].image+"')";
+        }
+    })
+}
+
+getData();
